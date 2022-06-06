584bfa9443b068bc7ccd73e8814add337ffba341
saeminkr <saeminkr@gmail.com>
2022-06-06 09:51:12 +0900

    Updates

---
+++ precache-manifest.4210bc54d4c040a6ff9800e6e067fb07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "676db811f616ea617161",
+    "url": "/pdfj-express/static/css/main.886666e9.chunk.css"
+  },
+  {
+    "revision": "676db811f616ea617161",
+    "url": "/pdfj-express/static/js/main.676db811.chunk.js"
+  },
+  {
+    "revision": "566e5359f79ea07cbc3f",
+    "url": "/pdfj-express/static/js/1.566e5359.chunk.js"
+  },
+  {
+    "revision": "bf245fc31af9e8ef4ab1",
+    "url": "/pdfj-express/static/js/runtime~main.bf245fc3.js"
+  },
+  {
+    "revision": "4a7073a9214a93338daa094049ffdba0",
+    "url": "/pdfj-express/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/pdfj-express/precache-manifest.4210bc54d4c040a6ff9800e6e067fb07.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/pdfj-express/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
